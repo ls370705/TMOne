afbf704600a3c3a10486e24d8ddfd0b72d2b3a12
Anita Chen <b02902044@ntu.edu.tw>
2017-01-02 02:12:39 +0800

    create list in AlertlistActivity

--- app/src/main/java/com/hcid/tmone/tmapp/alerts/AlertListActivity.java
+++ app/src/main/java/com/hcid/tmone/tmapp/alerts/AlertListActivity.java
@@ -1,6 +1,5 @@
 package com.hcid.tmone.tmapp.alerts;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,12 +9,13 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.hcid.tmone.tmapp.R;
 
-public class AlertListActivity extends Fragment implements OnClickListener {
+public class AlertListActivity extends Fragment {
 
     @Nullable
     @Override
@@ -23,21 +23,22 @@ public class AlertListActivity extends Fragment implements OnClickListener {
         View v = inflater.inflate(R.layout.activity_alert_list, container, false);
         setHasOptionsMenu(true);
 
-        Button b1 = (Button) v.findViewById(R.id.button1);
-        b1.setOnClickListener(this);
-
-        Button b2 = (Button) v.findViewById(R.id.button2);
-        b2.setOnClickListener(this);
+        ListView listView = (ListView)v.findViewById(R.id.listView);
+        final String[] arr = new String[] {"A","B","C","D","E","F","G"};
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,arr);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame, new AlertActivity());
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
 
         return v;
-    }
-
-    @Override
-    public void onClick(View v) {
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame, new AlertActivity());
-        transaction.addToBackStack(null);
-        transaction.commit();
     }
 
     @Override
